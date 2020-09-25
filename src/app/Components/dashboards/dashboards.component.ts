ef1f9efb989ebe29d716e6973e6d95cac6f4aacc
Alex Mendez Jacinto <lrs17110677@purisima.tecnm.mx>
2020-09-24 23:55:56 -0500

    App Proyecto_Parcial1

---
+++ src/app/Components/dashboards/dashboards.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboards',
+  templateUrl: './dashboards.component.html',
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
